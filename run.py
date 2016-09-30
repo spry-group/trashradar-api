db442b03d34b32d9443b63fa1e7b09264d94b722
marton987 <martin.jfreytes@gmail.com>
2016-09-30 15:57:25 -0500

    4 Adding user model

--- run.py
+++ run.py
@@ -1,5 +1,26 @@
 from eve import Eve
-app = Eve()
+from eve.auth import BasicAuth
+
+import bcrypt
+
+
+class BCryptAuth(BasicAuth):
+    def check_auth(self, username, password, allowed_roles, resource, method):
+        # use Eve's own db driver; no additional connections/resources are used
+        accounts = app.data.driver.db['accounts']
+        account = accounts.find_one({'username': username})
+        return account and \
+            bcrypt.hashpw(password.encode('utf-8'), account['salt'].encode('utf-8')) == account['password']
+
+
+def create_user(documents):
+    for document in documents:
+        document['salt'] = bcrypt.gensalt().encode('utf-8')
+        password = document['password'].encode('utf-8')
+        document['password'] = bcrypt.hashpw(password, document['salt'])
+
+app = Eve(auth=BCryptAuth)
+app.on_insert_accounts += create_user
 
 if __name__ == '__main__':
     app.run(debug=True)
