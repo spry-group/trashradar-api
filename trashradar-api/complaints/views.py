4d48d8d95a1904dd51729446b83afd7d1c88360c
marton987 <martin.jfreytes@gmail.com>
2017-02-24 00:12:18 +0000

    37 Adding Entity Viewset

--- trashradar-api/complaints/views.py
+++ trashradar-api/complaints/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from accounts.permissions import IsReadOnly
+from complaints import models, serializers
+
+
+class EntityViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for Accounts
+    """
+    queryset = models.Entity.objects.all()
+    serializer_class = serializers.EntitySerializer
+    permission_classes = (IsReadOnly,)
