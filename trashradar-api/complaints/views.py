94c8bbe3fa5cd5cf897f883a8f214f79eaadccf1
marton987 <martin.jfreytes@gmail.com>
2017-04-07 22:24:54 +0100

    23 Adding view to confirm trash place

--- trashradar-api/complaints/views.py
+++ trashradar-api/complaints/views.py
@@ -1,5 +1,6 @@
-
-from rest_framework import viewsets
+from rest_framework import permissions, status, viewsets
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
 
 from accounts.permissions import IsReadOnly
 from complaints import models, serializers
@@ -9,6 +10,13 @@ class ComplaintViewSet(viewsets.ModelViewSet):
     queryset = models.Complaint.objects.all()
     serializer_class = serializers.ComplaintSerializer
     permission_classes = (IsReadOnly,)
+
+    @detail_route(methods=['post'], permission_classes=[permissions.IsAuthenticated])
+    def confirm(self, request, pk):
+        complaint = self.get_object()
+        complaint.counter += 1
+        complaint.save()
+        return Response({}, status.HTTP_204_NO_CONTENT)
 
 
 class EntityViewSet(viewsets.ModelViewSet):
