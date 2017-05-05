e420569df8fac0cba7cfca8c74cc86c6a93557c1
Carlos Chitty <chitty.carlosj@gmail.com>
2017-05-05 15:55:39 -0400

    64 Use cloudinary, serialize picture and tests

--- trashradar-api/complaints/views.py
+++ trashradar-api/complaints/views.py
@@ -2,6 +2,11 @@ from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import detail_route
 from rest_framework.response import Response
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+from django.conf import settings
+
 from accounts.permissions import IsReadOnly
 from complaints import models, serializers
 
@@ -9,7 +14,29 @@ from complaints import models, serializers
 class ComplaintViewSet(viewsets.ModelViewSet):
     queryset = models.Complaint.objects.all()
     serializer_class = serializers.ComplaintSerializer
-    permission_classes = (IsReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    cloudinary.config(
+        cloud_name=settings.CLOUDINARY_CLOUD_NAME,
+        api_key=settings.CLOUDINARY_API_KEY,
+        api_secret=settings.CLOUDINARY_API_SECRET,
+    )
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            saved_image = cloudinary.uploader.upload(request.data.get('picture'))
+            url = saved_image.get('url', None)
+            if url:
+                picture = url
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response({
+            'status': 'Bad Request',
+            'message': 'Request failed validation.',
+            'errors': serializer.errors
+            }, status=status.HTTP_400_BAD_REQUEST)
 
     @detail_route(methods=['post'], permission_classes=[permissions.IsAuthenticated])
     def confirm(self, request, pk):
